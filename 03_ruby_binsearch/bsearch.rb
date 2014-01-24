8bc5ad4cbebe4cc9afd4a7dae5e6cad596bac497
george <rpubaddr0@gmail.com>
2014-01-23 20:14:18 -0700

    Working on binsearch (03).

--- 03_ruby_binsearch/bsearch.rb
+++ 03_ruby_binsearch/bsearch.rb
@@ -1,8 +1,7 @@
 class BSearch
   def chop(target, sorted_array)
     # Returns the index of the target if it exists in the list, 
-    # otherwise returns -1. (Even though you can just use list.target
-    # for this in real life.)
+    # otherwise returns -1.
     
   end
 end
