6a4da049549663cfc54389c263301dc67c6fba16
george <rpubaddr0@gmail.com>
2014-03-18 12:19:00 -0600

    More work on chapter 3.

--- 11_haskell_ch3/ChapterThree.hs
+++ 11_haskell_ch3/ChapterThree.hs
@@ -18,27 +18,57 @@ courseMajor course@(x:y:xs) = course ++ " is a " ++ [x] ++ [y] ++ " course"
 
 -- 3
 -- simple where clause
-
+threshold :: (Num a, Ord a) => a -> a -> a -> String
+threshold price qty size
+    | total < size   = "Total is low"
+    | total < medium = "Total is medium"
+    | total < high   = "Total is high"
+    | otherwise      = "Total is extrodinary"
+    where total = price * qty
+          medium = size + (size - 1)
+          high = medium + (size - 1)
 
 -- 4
 -- constants in where clause
-
+lactate :: (Fractional a, Ord a) => a -> a -> String
+lactate rate max
+    | rate <= easy*max        = "warmup"
+    | rate <= aerobic*max     = "aerobic"
+    | rate <= steadyState*max = "steadyState"
+    | rate <= anaerobic*max   = "anaerobic"
+    | otherwise               = "wow, don't do this for long!"
+    where easy = 0.6
+          aerobic = 0.7
+          steadyState = 0.8
+          anaerobic = 0.9
+          competitive = 1
 
 -- 5
 -- function definition in the where clause
-
+calcAreas :: (Num a) => [(a, a)] -> [a]
+calcAreas xs =
+    [ area x | x <- xs ]
+    where area pair = fst pair * snd pair
 
 -- 6
 -- function definition in the where clause
-
+calcTriangleAreas :: (Fractional a) => [(a, a)] -> [a]
+calcTriangleAreas xs =
+    [ triangleArea x | x <- xs ]
+    where triangleArea pair = fst pair * snd pair * 0.5
 
 -- 7
 -- guards
-
+orderTwo :: (Ord a) => [a] -> [a]
+orderTwo (x:y)
+    | x < y     = [x, y]
+    | otherwise = [y, x]
 
 -- 8
 -- two different ways
-
+orderThree :: (Ord a) => [a] -> [a]
+--orderThree (x:y:z:_)
+--    | 
 
 -- 9
 -- 
