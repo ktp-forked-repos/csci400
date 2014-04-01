42402edb3576630be61987b6063ef44c66cdec23
george <rpubaddr0@gmail.com>
2014-03-31 22:27:56 -0600

    Didn't finish h2ch5pt3

--- 16_haskell_ch5pt3/ChapterFive_Pt3.hs
+++ 16_haskell_ch5pt3/ChapterFive_Pt3.hs
@@ -1,40 +1,44 @@
-module Chapter5_Pt3 where
+module ChapterFive_Pt3 where
 
 -- 1
 getUserName :: String -> String
-
+getUserName xs = fst $ break ('@' ==) xs
 
 -- 2
 -- lambda expression
-numAs :: (Num a) => a -> Int
-
+numAs :: (Num a, Ord a) => [a] -> Int
+numAs xs = foldl (\acc x -> acc + 1) 0 $ filter (90<=) xs
 
 -- 3
 -- fold
 totalDiscount :: (Fractional a, Num a) => a -> [a] -> a
-
+totalDiscount x xs = foldl (\acc y -> acc + y*x) 0 xs
 
 -- 4
 -- fold
 totalWithDiscount :: (Fractional a, Num a) => a -> [a] -> a
+totalWithDiscount x xs = foldl (\acc y -> acc + y - y*x) 0 xs
 
 -- 5
--- fold
-discountedItems :: (Fractional a, Num a) => a -> [a] -> [a]
-
+discountedItems :: (Fractional a) => a -> [a] -> [a]
+discountedItems x = map (x*)
 
 -- 6
 -- fold
-anyBigNumbers :: (Num a) => a -> [a] -> Bool
+--anyBigNumbers :: (Num a) => a -> [a] -> Bool
+
 
 -- 7
--- 
-
+-- and_1 (repeat False) will immediately return False
+-- and_2 (repeat False) will run forever
+-- This is because foldr will only look at the first element before
+-- returning False, while foldl will try to start at the end of an
+-- infinite list.
 
 -- 8
 -- $
-multTableRow :: (Num a) => a -> [a]
-
+--multTableRow :: (Num a) => a -> [a]
+--multTableRow x :: 
 
 -- 9
 -- function composition
