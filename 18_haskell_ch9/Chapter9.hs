f37a1d4003cf2a33bd5767cbbb725fc4c3e20c60
george <rpubaddr0@gmail.com>
2014-04-08 11:56:08 -0600

    non-working code: working on hs ch9.

--- 18_haskell_ch9/Chapter9.hs
+++ 18_haskell_ch9/Chapter9.hs
@@ -1,4 +1,5 @@
 import System.IO
+import Data.Char
 
 -- 1
 pigLatin :: String -> String
@@ -10,12 +11,25 @@ pigLatin xs = if head xs `elem` "aeiou"
 piggly :: IO ()
 piggly = do
     hSetBuffering stdout LineBuffering  -- This is only needed by GHCI
-    interact $ unlines . map (unwords . map (pigLatin) . words) . lines
+    interact $ unlines . map (unwords . map pigLatin . words) . lines
 
 -- 3
-
+caesar :: Int -> Char -> Char
+caesar x y
+    | adj <= z && adj >= a = chr adj
+    | i <= z && i >= a     = 
+    | otherwise = 
+    where adj  = y + ord x
+          z    = ord 'z'
+          a    = ord 'a'
+          capZ = ord 'Z'
+          capA = ord 'A'
 
 -- 4
-
+encrypt :: Int -> IO ()
+encrypt x = do
+    hSetBuffering stdout LineBuffering  -- This is only needed by GHCI
+    interact $ map $ caesar x
 
 -- 5
+
